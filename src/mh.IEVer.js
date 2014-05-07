20bc8df876d8d1b547790dd20118f8140de3990f
exit <neiliux@gmail.com>
2014-05-07 14:51:23 -0700

    1) Add UserAgentService. 2) Added additional functions to IEVer service."

--- src/mh.IEVer.js
+++ src/mh.IEVer.js
@@ -1,15 +1,37 @@
-'use strict';
+"use strict";
 var mhIeVer = angular.module('mh.IEVer', [], null);
 
 mhIeVer.factory('IEVer', [
-	function () {
+	'userAgentService',
+	function(
+		userAgentService) {
 		return {
 			version: function() {
 				return '0.1';
 			},
 
-			foo: function() {
-				alert('xsdsdf');
+			userAgent: function() {
+				return userAgentService.getUserAgent();
+			},
+
+			isIE: function() {
+				// IE 11 seems to have removed 'msie' from the user agent,
+				// but we can use 'trident' to detect IE 11.
+				var regEx = /msie|trident/i;
+				var userAgent = userAgentService.getUserAgent();
+				return regEx.test(userAgent);
+			},
+
+			IEMajorVersion: function() {
+				var userAgent = userAgentService.getUserAgent();
+				var match = userAgent.match(/(?:msie |rv:)(\d+(\.\d+)?)/i);
+				var version = (match && match.length > 1 && match[1]) || null;
+
+				if (version == null ||
+					version.indexOf('.') == -1) {
+					return null;
+				}
+				return parseInt(version.split('.')[0]);
 			}
 		};
 	}
